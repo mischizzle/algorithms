38dc371102d243511d43a42de06697d787907ae3
Michelle Anderson <michelle@picot.ca>
2015-10-09 13:04:16 +0100

    Adding index html file to debug with dev tools. to do: implement the merge verification for the case where there are uneven number of merged array items; loop through, merge itself

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -3,81 +3,62 @@
  */
 
 function mergeSort(arr) {
-  var middle,
-      firstHalf,
-      secndHalf;
+  //console.log("merge sort array:", arr);
+  var middle, left, right, l, r;
 
   if (arr.length === 1) {
+    //console.log("array length is only 1, return", '\n');
     return arr;
   }
 
   middle = Math.floor(arr.length/2);
-  firstHalf = arr.slice(0, middle);
-  secndHalf = arr.slice(middle, arr.length);
+  left = arr.slice(0, middle);
+  right = arr.slice(middle, arr.length);
 
-  console.log(middle);
-  console.log("first half:", firstHalf, "second half:", secndHalf);
+  l = mergeSort(left);
+  r = mergeSort(right);
 
-  return merge(firstHalf, secndHalf);
+  return merge(l, r);
 }
 
-/** Pseudocode:
- for k=1 to n
-   if A(i) < B(j)
-     C(k) = A(i)
-     i++
-   else
-     [B(j) < A(i)]
-     C(k) = B(j)
-     j++
-**/
 function merge(left, right) {
   var i = 0,
-      j = 0;
-      k = 0;
+      j = 0,
+      k = 0,
+      mergedArray = [];
 
   //merging the 2 sub arrays into answer
   while(i <= left.length && j <= right.length) {
-      if(left[i] < right[j]) {
-        answer.push(left[i]);
-        i++;
-      } else {
-        answer.push(right[j]);
-        j++;
-      }
-      k++;
+    if(left[i] < right[j]) {
+      answer.push(left[i]);
+      i++;
+    } else {
+      answer.push(right[j]);
+      j++;
+    }
+    k++;
   }
 
   //taking care of the case where there are leftovers
   //in left
-  while() {
-
-  }
+  //while() {}
 
   //in right
-  while() {
-
-  }
-
-  return answer;
-
-
-  //for(var k=0; k<left.length; k++) {
-  //  if(left[i] < right[j]) {
-  //    answer.push(left[i]);
-  //    i++;
-  //  } else {
-  //    answer.push(right[j]);
-  //    j++;
-  //  }
-  //}
-  //console.log("answer:", answer);
-  //return answer;
+  //while() {}
+  return mergedArray;
 }
 
 var answer = [];
 var testArr = [5, 4, 1, 8, 7, 2, 6, 3];
-var mergeSort = mergeSort(testArr);
+mergeSort(testArr);
 
-console.log("final merge sort:", mergeSort);
-
+/** Pseudocode:
+ for k=1 to n
+ if A(i) < B(j)
+ C(k) = A(i)
+ i++
+ else
+ [B(j) < A(i)]
+ C(k) = B(j)
+ j++
+ **/
