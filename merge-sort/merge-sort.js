569577eb4141faad8fbf2772662f73d390c66ee2
Michelle Anderson <michelle@picot.ca>
2015-10-09 14:22:12 +0100

    Completed algorithm

--- merge-sort/merge-sort.js
+++ merge-sort/merge-sort.js
@@ -3,59 +3,67 @@
  */
 
 function mergeSort(arr) {
-  //console.log("merge sort array:", arr);
-  var middle, left, right, l, r;
+  var middle,
+      leftArr,
+      rightArr,
+      left,
+      right;
 
   if (arr.length === 1) {
     return arr;
   }
 
   middle = Math.floor(arr.length/2);
-  left = arr.slice(0, middle);
-  right = arr.slice(middle, arr.length);
+  leftArr = arr.slice(0, middle);
+  rightArr = arr.slice(middle, arr.length);
 
-  l = mergeSort(left);
-  r = mergeSort(right);
+  /** Calling merge sort again will divide into sub arrays until only 1 or 0 leaf nodes; **/
+  left = mergeSort(leftArr);
+  right = mergeSort(rightArr);
 
-  return merge(l, r);
-}
+  return _merge(left, right);
 
-function merge(left, right) {
-  var i = 0,
-      j = 0,
-      k = 0,
-      mergedArray = [];
+  /** local function **/
+  function _merge(left, right) {
+    var i = 0,
+        j = 0,
+        mergedArray = [];
 
-  //merging the 2 sub arrays into answer
-  while(i <= left.length && j <= right.length) {
-    if(left[i] <= right[j]) {
+    /** merging the 2 sub arrays into answer **/
+    while(i < left.length && j < right.length) {
+      if(left[i] <= right[j]) {
+        mergedArray.push(left[i]);
+        i++;
+      } else {
+        mergedArray.push(right[j]);
+        j++;
+      }
+    }
+
+    /** taking care of the case where there are leftovers **/
+    //in left array
+    while(i < left.length) {
       mergedArray.push(left[i]);
       i++;
-    } else {
+    }
+
+    //in right array
+    while(j < right.length) {
       mergedArray.push(right[j]);
       j++;
     }
-    k++;
-  }
 
-  /** taking care of the case where there are leftovers **/
-  //in left array
-  while(i <= left.length) {
-    mergedArray.push(left[i]);
-    i++;
+    return mergedArray;
   }
-
-  //in right array
-  while(j <= right.length) {
-    mergedArray.push(left[j]);
-    j++;
-  }
-
-  return mergedArray;
 }
 
+
 var testArr = [5, 4, 1, 8, 7, 2, 6, 3];
-mergeSort(testArr);
+var answer = mergeSort(testArr);
+
+console.log(testArr);
+console.log("--- will be sorted into: ---");
+console.log(answer);
 
 /** Pseudocode:
  for k=1 to n
